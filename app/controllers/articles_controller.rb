fc8409e45b73f7093da086f6c628b9b970f7e1e7
JoaoAlexandre <joao.contact@icloud.com>
2014-11-25 18:47:45 -0200

    Sort Articles by Newest

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   before_filter :require_login, except: [:index, :show]
 
 	def index
-		@articles = Article.all
+		@articles = Article.order('created_at DESC')
 	end
 
 	def show
